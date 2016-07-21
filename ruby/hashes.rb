7d7bfa86f3b66bcc336f5d82a45f25d593bbc55b
Kevin Kordestani <kevin.kordestani@gmail.com>
2016-07-20 22:29:14 -0700

    pseudocode for interior design client

---
+++ ruby/hashes.rb
@@ -0,0 +1,10 @@
+#Release 3
+
+# delcare a hash for interior desiner client
+#prompt designer for clients info(name, age, number of children, decor theme,)
+#after each promp use gets.chomp
+#add each client detail to hash 
+#ask user to update any data 
+#print out hash and exit program 
+
+
