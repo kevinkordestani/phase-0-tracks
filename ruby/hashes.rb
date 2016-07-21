4f9fbfdb2baef1d6d82dbc0f44828394b1d8dce9
Kevin Kordestani <kevin.kordestani@gmail.com>
2016-07-20 22:44:36 -0700

    prompted user to enter client data and saved data to hash

--- ruby/hashes.rb
+++ ruby/hashes.rb
@@ -7,4 +7,29 @@
 #ask user to update any data 
 #print out hash and exit program 
 
+design_clients = {}
+puts "Client Data:"
+puts "Please enter client name"
+name = gets.chomp 
+design_clients[:name] = "#{name}"
 
+puts "Please enter clients age"
+age = gets.chomp.to_i
+design_clients[:age] = "#{age}"
+
+puts "Please enter number of children"
+children = gets.chomp.to_i
+design_clients[:children] = "#{children}"
+
+puts "Likes minimalism? (y/n)"
+minimalism = gets.chomp
+	if minimalism == "y"
+		design_clients[:minimalism] = true
+	else 
+		design_clients[:minimalism] = false
+	end
+puts "Decor Theme"
+theme = gets.chomp
+design_clients[:theme] = "#{theme}"
+
+p design_clients
