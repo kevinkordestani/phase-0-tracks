4d4056f3182b01978d0db8130896855f8e87baf4
Kevin Kordestani <kevin.kordestani@gmail.com>
2016-07-21 22:45:01 -0700

    iterated through array and hash

--- ruby/iteration.rb
+++ ruby/iteration.rb
@@ -6,3 +6,31 @@ def play_music
 end
 
 play_music {|song, artist| puts "I would like to play, #{song}, by the #{artist}."}
+
+
+#declare an array and a hash and iterate through each one using .each and .map!
+colors = ["red", "gold", "purple"]
+p colors
+
+colors.each do |color|
+	puts "#{color} is a color of one my favorite sports teams"
+end
+
+colors.map! do |color|
+	color = "black & yellow"
+end
+p colors 
+
+actor_character = {
+	christian_bale: "Batman",
+	robert_downey_jr: "Tony Stark",
+	johnny_depp: "Captain Jack Sparrow"
+}
+
+actor_character.each do |actor, character|
+	puts "#{actor} plays #{character}."
+end
+p actor_character
+
+
+
