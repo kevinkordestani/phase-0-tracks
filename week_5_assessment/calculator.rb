6b857f17b6dddb19ae900169561277d00a46e743
Kevin Kordestani <kevin.kordestani@gmail.com>
2016-07-24 22:05:25 -0700

    questions 3-5

--- week_5_assessment/calculator.rb
+++ week_5_assessment/calculator.rb
@@ -14,4 +14,26 @@ end
 p calculate(1, "+", 1)
 p calculate(1, "-", 1)
 p calculate(2, "*", 3)
-p calculate(6, "/", 2)
+p calculate(6, "/", 2) 
+
+calculations = {}
+loop do 
+
+	puts "please enter a calculation"
+	math_problem = gets.chomp
+	problem = math_problem.split('')
+	first_number = problem[0].to_i
+	operator = problem[1]
+	second_number = problem[2].to_i
+
+	answer = calculate(first_number, operator, second_number)
+
+	p answer
+	break if math_problem == "done"
+	calculations[math_problem] = answer
+end
+
+puts "#{calculations.length} calculations performed:"
+calculations.each do |math_problem, answer|
+	puts "#{math_problem} = #{answer}"
+end
