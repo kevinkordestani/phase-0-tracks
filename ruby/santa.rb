150a46ab7fa8b1ce565a940f7cae07286a7cca88
Kevin Kordestani <kevin.kordestani@gmail.com>
2016-07-25 09:54:57 -0700

    release 0

---
+++ ruby/santa.rb
@@ -0,0 +1,19 @@
+#Release 0 
+
+class Santa 
+	def speak 
+		puts "Ho, ho, ho! Haaaappy holidays!"
+	end
+
+	def eat_milk_and_cookies(cookie_type)
+		puts "That was a good #{cookie_type} cookie!"
+	end
+
+	def initialize
+		puts "Initializing Santa instance ..."
+	end
+end
+
+saint_nick = Santa.new 
+saint_nick.speak
+saint_nick.eat_milk_and_cookies("chocolate chip")
