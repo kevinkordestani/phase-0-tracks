d40e86d4db8e70b9d1c62724930f94ea0fb9ea7b
Kevin Kordestani <kevin.kordestani@gmail.com>
2016-07-25 00:31:37 -0700

    5.6

---
+++ ruby/problem_solving_methods.rb
@@ -0,0 +1,22 @@
+
+#Release 0
+def search_array(array, integer)
+	index = 0 
+	while index < array.length 
+		if array[index] == integer 
+			p integer
+		else
+			p nil
+		end
+		index += 1
+	end
+
+end
+
+array = [1, 2, 3, 4]
+p search_array(array, 3)
+
+#Release 1 
+
+
+	
