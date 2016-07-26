be1f09bdc5b39352093eb36d9d7ed3bd9a5a4f63
Kevin Kordestani <kevin.kordestani@gmail.com>
2016-07-25 23:27:46 -0700

    Mix in module and convert standalone module to a mixin

--- ruby/shout.rb
+++ ruby/shout.rb
@@ -1,12 +1,28 @@
 module Shout
-	def self.yell_angrily(words)
-		words + "!!!" + " :("
+	def yell_angrily(words)
+		words + "!!!" + " >:("
 	end
 
-	def self.yelling_happily(words)
+	def yelling_happily(words)
 		words + "!!!!!" + " :))"
 	end
 end
 
-p Shout.yell_angrily("I AM SO ANGRY")
-p Shout.yelling_happily("I AM SO HAPPY")
+#p Shout.yell_angrily("I AM SO ANGRY")
+#p Shout.yelling_happily("I AM SO HAPPY")
+
+class Teacher 
+	include Shout
+end
+
+class Coach
+	include Shout
+end
+
+mrs_bloomfield = Teacher.new
+p mrs_bloomfield.yell_angrily("Quiet in the classroom")
+p mrs_bloomfield.yelling_happily("Congrats, you graduated")
+
+coach_boone = Coach.new
+p coach_boone.yell_angrily("DROP AND GIVE ME 50")
+p coach_boone.yelling_happily("WE ARE THE CHAMPIONS")
