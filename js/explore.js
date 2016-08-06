641b1ecdbe7a40b48600e2321872bcf5a4301bab
Kevin Kordestani <kevin.kordestani@gmail.com>
2016-08-05 17:25:04 -0700

    pseudocode for reverse string function

---
+++ js/explore.js
@@ -0,0 +1,9 @@
+//create empty string which will be the new reversed string 
+//loop through each letter of the string
+//	set our counter to str.length-1 which will start with last letter of string
+// 	we decrease i after each iteration
+// return the reversed string
+
+
+
+
